cfa70c3437630410a34b97ed0e1dacdf1ef179df
Anton Krutoy <2191@mail.ru>
2024-07-02 13:27:49 +0500

    new 02-07-2024

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,48 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun calculateCommission(
+    cardType: String = "Мир",
+    previousTransfersAmount: Double = 0.0,
+    transferAmount: Double
+): Double {
+    // Проверяем лимиты на сумму перевода
+    if (transferAmount > 150000) {
+        throw IllegalArgumentException("Превышен суточный лимит перевода")
+    }
+    if (previousTransfersAmount + transferAmount > 600000) {
+        throw IllegalArgumentException("Превышен месячный лимит перевода")
+    }
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    // Рассчитываем комиссию в зависимости от типа карты
+    val commission: Double = when (cardType) {
+        "Mastercard" -> {
+            if (previousTransfersAmount + transferAmount > 75000) {
+                transferAmount * 0.006 + 20
+            } else {
+                0.0
+            }
+        }
+
+        "Visa" -> {
+            maxOf(transferAmount * 0.0075, 35.0)
+        }
+
+        "Мир" -> {
+            0.0
+        }
+
+        else -> {
+            throw IllegalArgumentException("Неизвестный тип карты")
+        }
+    }
+
+    return commission
+}
+
+
+fun main() {
+    val commission = calculateCommission(
+        cardType = "Mastercard",
+        previousTransfersAmount = 70000.0,
+        transferAmount = 100000.0
+    )
+    println("Сумма комиссии: $commission рублей")
 }
--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -1,10 +1,87 @@
-import org.junit.Test
-
 import org.junit.Assert.*
+import org.junit.Test
 
 class MainKtTest {
 
     @Test
     fun calculateCommission() {
+        // val cardType: String = "Мир"
+        val cardType: String = "Visa"
+        val previousTransfersAmount: Double = 60_000.0
+        val transferAmount: Double = 120_000.0
+
+        // Проверяем лимиты на сумму перевода
+        if (transferAmount > 150_000) {
+            throw IllegalArgumentException("Превышен суточный лимит перевода")
+        }
+        if (previousTransfersAmount + transferAmount > 600000) {
+            throw IllegalArgumentException("Превышен месячный лимит перевода")
+        }
+
+        // Рассчитываем комиссию в зависимости от типа карты
+        val commission: Double = when (cardType) {
+            "Mastercard" -> {
+                if (previousTransfersAmount + transferAmount > 75000) {
+                    transferAmount * 0.006 + 20
+                } else {
+                    0.0
+                }
+            }
+
+            "Visa" -> {
+                maxOf(transferAmount * 0.0075, 35.0)
+            }
+
+            "Мир" -> {
+                0.0
+            }
+
+            else -> {
+                throw IllegalArgumentException("Неизвестный тип карты")
+            }
+        }
+
+        assertEquals(900.0, commission, 0.1)
+    }
+
+    @Test
+    fun calculateCommission111() {
+        // val cardType: String = "Мир"
+        val cardType: String = "Visa"
+        val previousTransfersAmount: Double = 60_000.0
+        val transferAmount: Double = 120_000.0
+
+        // Проверяем лимиты на сумму перевода
+        if (transferAmount > 150_000) {
+            throw IllegalArgumentException("Превышен суточный лимит перевода")
+        }
+        if (previousTransfersAmount + transferAmount > 600000) {
+            throw IllegalArgumentException("Превышен месячный лимит перевода")
+        }
+
+        // Рассчитываем комиссию в зависимости от типа карты
+        val commission: Double = when (cardType) {
+            "Mastercard" -> {
+                if (previousTransfersAmount + transferAmount > 75000) {
+                    transferAmount * 0.006 + 20
+                } else {
+                    0.0
+                }
+            }
+
+            "Visa" -> {
+                maxOf(transferAmount * 0.0075, 35.0)
+            }
+
+            "Мир" -> {
+                0.0
+            }
+
+            else -> {
+                throw IllegalArgumentException("Неизвестный тип карты")
+            }
+        }
+
+        assertEquals(90.0, commission, 0.1)
     }
 }
